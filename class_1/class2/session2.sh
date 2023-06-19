49d44ccaf79973d59bd95cb5f608de102a8d6673
Umidbekakumo <umidbek2022@gmail.com>
2023-06-19 23:45:23 +0000

    session2.sh

---
+++ class_1/class2/session2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo yum update rpm -y 
+sudo yum update -y 
+
+sudo yum install git -y 
+
+git config --global user.name "Umidbekakumo"
+git config --global user.email "Umidbek2022@gmail.com" 
+
+git clone https://github.com/Umidbekakumo/scripting1.git /root/scripting_1
+
+
+
+
