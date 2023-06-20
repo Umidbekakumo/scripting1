f2e0aedc723156eb70876666f26e12d903754201
Umidbekakumo <Umidbek2022@gmail.com>
2023-06-20 02:00:23 +0000

    committing class work

---
+++ class_2/script3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "What is your name cuz? : " name
+
+if [ $name = "Umut" ]; then 
+echo "Welcome back Umut wayne aka Batman! im going to get your batmobile "
+else
+echo " youre not batman leave $name! scadaddle!! "
+
+fi
+
+
